8452db07fa637ff09a69b1ef41fa8ed7fb197916
karmic <buted@hawaii.edu>
2011-02-27 16:12:58 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libraryrailshcc::Application.config.secret_token = '067e0fc2cf4df93a8a407046e5a6752f990362088df52ee41fe67976311c81a2ca0ebea1ace582fc226dcc7a0c338d6c3ffb5af5684444c8c0497e5ceccad8e1'
